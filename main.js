3bdd99a4874f94402e70b1f5be75843585d319d5
Cscott84 <45050662+Cscott84@users.noreply.github.com>
2019-05-03 16:47:53 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
-function calc(){
+function calc() {
     let a = parseInt(document.getElementById("impressions").value);
     let b = parseInt(document.getElementById("cost").value);
     let c = parseInt(document.getElementById("cpm").value);
-    
     //computations for cpm, cost and impressions
+
+    let cpm = b / a * 1000;
+    let impr = (b * 1000) / c;
+    let cost = c * a / 1000;
     
-    document.getElementById("cpm").value = b/a * 1000;
-    document.getElementById("impressions").value = (b *1000) / c;
-    document.getElementById("cost"). value= c *a /1000;
-    
-    if( c > 10.00){
+    document.getElementById("impressions").value = impr || a;
+    document.getElementById("cost").value = cost || b;
+    document.getElementById("cpm").value = cpm || c;
+
+    // When cpm hits $10 threshold - warning will be red background and warning text will appear    
+    if (cpm > 10.00) {
         document.getElementById("div1").style.background = "red";
+        document.getElementById("results").style.display = "block";
     }
+     else if (cpm < 10.00) {
+        document.getElementById("div1").style.background = "orange";
+        document.getElementById("results").style.display = "none";
+    }
+
+    
     
 }
+
