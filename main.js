3744349411f87a25ce8dd2f97e78e0e76547718e
Cscott84 <45050662+Cscott84@users.noreply.github.com>
2019-05-09 14:41:19 -0400

    file updated 5.9.2019

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ function calc() {
     let a = parseInt(document.getElementById("impressions").value);
     let b = parseInt(document.getElementById("cost").value);
     let c = parseInt(document.getElementById("cpm").value);
+   
     //computations for cpm, cost and impressions
 
     let cpm = b / a * 1000;
@@ -12,14 +13,15 @@ function calc() {
     document.getElementById("cost").value = cost || b;
     document.getElementById("cpm").value = cpm || c;
 
-    // When cpm hits $10 threshold - warning will be red background and warning text will appear    
+   // When cpm hits $10 threshold - warning will be red background and warning text will appear    
+   
     if (cpm > 10.00) {
         document.getElementById("div1").style.background = "red";
         document.getElementById("results").style.display = "block";
     }
-     else if (cpm < 10.00) {
-        document.getElementById("div1").style.background = "orange";
-        document.getElementById("results").style.display = "none";
+    else if (cpm < 10.00) {
+        document.getElementById("div1").style.background = "#19B335";
+        document.getElementById("warning").style.display = "none";
     }
 
     
